5e4f4900900b38d2583ccbb31dff181259a56a69
Shihabyooo <shihabnoor@hotmail.com>
2020-05-30 19:09:19 +0200

    Added testing building menu.

---
+++ Assets/Scripts/BuildingMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMenu : MonoBehaviour
+{
+   
+   
+   public void SelectBuilding(int id)
+   {
+       //the code to translate the ID into a building goes here. For now, we hardcode block spawning just to test it out.
+       GameManager.gameMan.SwitchToBuildingPlacement(id);
+   }
+}
