e0b3b073be44d8668524294b30fd0fb6fffbd8c9
Shihabyooo <shihabnoor@hotmail.com>
2020-05-30 19:08:48 +0200

    Added a script dedicated to buildings, building stats.,

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BuildingType //TODO add remaining types.
+{
+    residential,
+}
+
+public class Building : MonoBehaviour
+{
+    //TODO switch those variables to private set.
+    public GameObject model;
+    public BuildingStats stats;
+
+}
+
+[System.Serializable]
+public class BuildingStats
+{
+    public int id;
+    public uint cost;
+    public BuildingType type;
+    //TODO add remaining -universal- parameters here.
+
+}
