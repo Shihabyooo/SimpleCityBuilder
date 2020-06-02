c210f3c79441edea137e0e6d5bf9562e90632838
Shihabyooo <shihabnoor@hotmail.com>
2020-06-02 09:16:41 +0200

    Simplified ProposedBuilding class

--- Assets/Scripts/BuildingsManager.cs
+++ Assets/Scripts/BuildingsManager.cs
@@ -39,26 +39,23 @@ public class BuildingsManager : MonoBehaviour
     public class BuildingProposal
     {
         public BuildingsManager buildingsManRef {get; private set;}
-        public BuildingStats targetBuildingStats {get; private set;}
-        public GameObject targetBuildingAvatar {get; private set;}
+        public GameObject targetBuilding {get; private set;}
 
         public BuildingProposal(int _targetBuildingID, BuildingsManager _buildingsManager)
         {
             buildingsManRef = _buildingsManager;
-            targetBuildingStats = buildingsManRef.database.GetStatsForBuilding(_targetBuildingID);
 
             //TODO once a mesh loader (or dedicated prefabs are created) for the mock avatar for the buildings, replace the line bellow.
             GameObject newProposedBuilding = buildingsManRef.database.GetBuildingObject(_targetBuildingID).gameObject;
-            targetBuildingAvatar = GameObject.Instantiate(newProposedBuilding);
-            targetBuildingAvatar.name = "TargetBuildingAvatar"; //test
+            targetBuilding = GameObject.Instantiate(newProposedBuilding);
 
-            if (targetBuildingAvatar == null)
+            if (targetBuilding == null)
             print ("WARNING! targetBuildingProposal is set to null, meaning no building of provided ID could be found.");
         }
 
       public bool CanConstructHere(Cell cell)
        {
-          if (cell.isOccupied)
+          if (cell.isOccupied || !targetBuilding.GetComponent<Building>().CheckResourceRequirements(cell))
                return false;
 
            //TODO add logic to assess whether positition supports building of this type here.
@@ -73,14 +70,10 @@ public class BuildingsManager : MonoBehaviour
             
             //TODO handle object construction here (add to waiting queue, update relevant databases, etc)    
             Grid.grid.SetCellOccupiedState(cell, true);
-            //TODO consider instead of instantiating a new building, just use targetBuildingAvatar (and rename it).
-            GameObject newBuilding = GameObject.Instantiate(  buildingsManRef.database.GetBuildingObject(targetBuildingStats.id),
-                                                            targetBuildingAvatar.transform.position,
-                                                            targetBuildingAvatar.transform.rotation);
-            newBuilding.name = "NewBuilding: " + newBuilding.name;
-            newBuilding.GetComponent<Building>().BeginConstruction();
-            buildingsManRef.AddConstructedBuilding(newBuilding.GetComponent<Building>());
-            Destroy(targetBuildingAvatar);
+            
+            buildingsManRef.AddConstructedBuilding(targetBuilding.GetComponent<Building>());
+            targetBuilding.GetComponent<Building>().BeginConstruction();
+            targetBuilding = null;
                 
             return true;
         }
@@ -88,14 +81,14 @@ public class BuildingsManager : MonoBehaviour
         public void Cancel()
         {
             //if (targetBuildingAvatar != null) //In a complete implementation, calling this method shouldn't be possible without a targetBuildingAvatar set.
-            Destroy (targetBuildingAvatar);
+            Destroy (targetBuilding);
         
             buildingsManRef.currentProposal = null;
         }
 
         public void MovePlan(Vector3 positition)
         {
-            targetBuildingAvatar.transform.position = positition;
+            targetBuilding.transform.position = positition;
         }
     }
 }
