304c06cbad38e89ae1afc46bf566147acc5b012a
Shihabyooo <shihabnoor@hotmail.com>
2020-06-02 15:02:42 +0200

    Added script for WaterTreatmentPlant, now update infrastructureLayer upon construction, minor additions.

--- Assets/Scripts/Management/SimulationManager.cs
+++ Assets/Scripts/Management/SimulationManager.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public class SimulationManager : MonoBehaviour
 {
     
+    //TODO divide the simulation to several runs. Example:
+        //Resident Happiness run.
+        //Infrastructure Use run: Loop over residents/buildings, update load on infrastructure building accordingly.
+        //Natural resources run: Loop over natural resource consuming building, based on their consumption, update natural resources.
+        //Budget run: Loop over everything relevant, compute delta on player budget.
+
 }
