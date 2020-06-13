198cadb25ff7eed8593ad3a76f4bc1d8cfb7d54b
Shihabyooo <shihabnoor@hotmail.com>
2020-06-13 17:59:51 +0200

    Implemented pan and orbit functionality in camera, added boundaries constraining camera movements

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -6,55 +6,61 @@ using UnityEngine;
 //TODO after implementing the Region class replacement, update this class
 public class CameraControl : MonoBehaviour
 {
-    public static CameraControl mainCam;
-    Transform focus;
-    //public Region minZoomRegion;
-    //public Region maxZoomRegion;
-    //Region regionCurrent;
-    public float zoomSpeed = 65.0f; 
-    public float cameraSpeed = 5.0f;
-
-    void Awake()
-    {
-        if (mainCam == null)
-            mainCam = this;
-        else
-            Destroy(this.gameObject);
-
-        focus = this.transform.Find("Focus");
-        //regionCurrent = this.gameObject.GetComponent<Region>();
-    }
-
-    // public void Zoom(float zoomRate)
-    // {
-    //     float zLevel = this.transform.position.z;
-    //     zLevel += zoomRate * zoomSpeed * Time.deltaTime;
-    //     //zLevel = Mathf.Clamp (zLevel, minZoomRegion.transform.position.z, maxZoomRegion.transform.position.z);
-
-    //     Vector3 newPos = this.transform.position;
-    //     newPos.z = zLevel;
-
-    //     this.transform.position = newPos;
-    //     Move(Vector3.zero); //Move() function has an OOB check and clamps camera location to within the regionCurrent. Since zooming out reduces the currentRegion, we call with
-    //                         //a zero-valued Vector to force clamping. This avoids an unpleasant jump that would happen if one zooms out with camera at edge of region, then moves.
-    // }
-
-    // public void Move(Vector3 direction)
-    // {
-    //     Vector3 newPos = this.transform.position;
-    //     newPos += direction * cameraSpeed * Time.deltaTime;
-
-    //     //compute the region based on the current zLevel from the min/maxZoomRegion
-    //     //regionCurrent.Average(maxZoomRegion, minZoomRegion, this.transform.position.z);
-    //     //newPos.x = Mathf.Clamp(newPos.x, regionCurrent.cornerSW.x, regionCurrent.cornerNE.x);
-    //     //newPos.y = Mathf.Clamp(newPos.y, regionCurrent.cornerSW.y, regionCurrent.cornerNE.y);
-        
-    //     this.transform.position = newPos;
-    // }
-
+    [SerializeField] Transform focus; //assigned in editor. //TODO consider double checking this in void Awake()
+    [SerializeField] float zoomSpeed = 65.0f; 
+    [SerializeField] float cameraSpeed = 5.0f;
+    [SerializeField] LayerMask cameraConstrainingLayers; //layers that the camera won't zoom past.
     [SerializeField] float maxCameraAngle = 70.0f;
     [SerializeField] float minCameraAngle = 10.0f;
     [SerializeField] float degreesPerTick = 5.0f;
+    [SerializeField] CameraBoundary cameraBoundary = new CameraBoundary();
+
+    public void Zoom(float zoomRate)
+    {
+        float hitDist = zoomSpeed * Time.deltaTime + 0.5f;
+        Ray ray = new Ray(this.transform.position, this.transform.forward);
+        RaycastHit hit;
+        bool isHit = Physics.Raycast(ray, out hit, hitDist, cameraConstrainingLayers);
+
+        if (isHit && zoomRate > 0.0f)
+            return;
+        
+        Vector3 moveDist = zoomRate * this.transform.forward * zoomSpeed * Time.deltaTime;
+        //print (moveDist);
+        this.transform.position += zoomRate * this.transform.forward * zoomSpeed * Time.deltaTime;
+
+        //Clam the position to within the boundaries
+        this.transform.position = cameraBoundary.Clamp(this.transform.position);
+    }
+
+    public void Move(Vector3 direction)
+    {
+        this.transform.Translate(direction * cameraSpeed * Time.deltaTime, Space.Self);
+    }
+
+    public void Pan(Vector2 direction)
+    {
+        //We pan over the X-Z plane. The problem is that we can't do this relative the the raw state of the object's space, because the rotation about the X-axis (camera tilt)
+        //does similarily to our plane, but we do need to maintain the rotation about the Y-axis as well, so we can't use the world space.
+        //A stupid hack: rotate set rotation over the X-axis to zero before translating, then reset it again. Stupid, but works...
+
+        //Convert the recieved X and Y values to X and Z values (with the new Y being zero).
+        Vector3 panDirection = Vector3.zero;
+        panDirection.x = direction.x;
+        panDirection.z = direction.y;
+        
+        //Backup the rotation.
+        Quaternion originalRot = this.transform.rotation;
+        //Set rotation about X-axis to zero.
+        this.transform.eulerAngles = new Vector3(0.0f, this.transform.eulerAngles.y, this.transform.eulerAngles.z); 
+        //translate (pan) the camera.
+        this.transform.Translate(panDirection * cameraSpeed * Time.deltaTime, Space.Self );
+        //Clam the position to within the boundaries
+        this.transform.position = cameraBoundary.Clamp(this.transform.position);
+        //rest rotation.
+        this.transform.rotation = originalRot;
+    }
+
     public void RotateView(Vector3 rotation, Vector3 rotationOrigin)
     {
         //Update focus position to the rotationOrigin, and update its axes so the rotation about the horizontal axes works properly.
@@ -69,7 +75,60 @@ public class CameraControl : MonoBehaviour
         this.transform.RotateAround(rotationOrigin, Vector3.up, rotation.x * degreesPerTick); //rotation around the vertical axes is done to world up.
         this.transform.RotateAround(rotationOrigin, focus.right, rotation.y * degreesPerTick);
 
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, this.transform.eulerAngles.y, 0.0f);
+        
+        //Clam the position to within the boundaries
+        this.transform.position = cameraBoundary.Clamp(this.transform.position);
     }
 
+    public RaycastHit CastRay(LayerMask layerMask, float distance)
+    {
+        Ray ray = new Ray(this.transform.position, this.transform.forward);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit, distance, cameraConstrainingLayers);
 
+        return hit;
+    }
+
+    //testing visualization
+    void OnDrawGizmos()
+    {        
+        Gizmos.color = Color.red;
+
+        Vector3 size = new Vector3(cameraBoundary.maxCornerXYZ.x - cameraBoundary.minCornerXYZ.x, cameraBoundary.maxCornerXYZ.y - cameraBoundary.minCornerXYZ.y, cameraBoundary.maxCornerXYZ.z - cameraBoundary.minCornerXYZ.z);
+        Vector3 centre = new Vector3((cameraBoundary.maxCornerXYZ.x + cameraBoundary.minCornerXYZ.x) / 2.0f, (cameraBoundary.maxCornerXYZ.y + cameraBoundary.minCornerXYZ.y)/ 2.0f, (cameraBoundary.maxCornerXYZ.z + cameraBoundary.minCornerXYZ.z)/ 2.0f);
+
+        Gizmos.DrawWireCube(centre ,size);
+        
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            Gizmos.DrawCube(focus.position, new Vector3(0.5f, 0.5f, 0.5f));
+        }
+    }
+}
+
+[System.Serializable]
+public class CameraBoundary 
+{
+    [SerializeField] public Vector3 minCornerXYZ = new Vector3();
+    [SerializeField] public Vector3 maxCornerXYZ = new Vector3();
+
+
+    public bool isOOB(Vector3 position)
+    {
+        if (position.x <= maxCornerXYZ.x && position.x >= minCornerXYZ.x
+            && position.y <= maxCornerXYZ.y && position.y >= minCornerXYZ.y
+            && position.z <= maxCornerXYZ.z && position.z >= minCornerXYZ.z)
+            return true;
+
+        return false;
+    }
+
+    public Vector3 Clamp(Vector3 position)
+    {
+        return new Vector3(Mathf.Clamp(position.x, minCornerXYZ.x, maxCornerXYZ.x),
+                            Mathf.Clamp(position.y, minCornerXYZ.y, maxCornerXYZ.y),
+                            Mathf.Clamp(position.z, minCornerXYZ.z, maxCornerXYZ.z));
+    }
+    
 }
--- Assets/Scripts/Management/CursorHandler.cs
+++ Assets/Scripts/Management/CursorHandler.cs
@@ -11,14 +11,17 @@ public enum ControlMode
 public class CursorHandler : MonoBehaviour
 {
     [SerializeField] ControlMode currentCursorMode; //TODO remove serialization after testing is done.
-    public LayerMask gridLayer;
-    public LayerMask ground;
-    public LayerMask freeModeSelectables;
+    [SerializeField] LayerMask gridLayer;
+    [SerializeField] LayerMask ground;
+    [SerializeField] LayerMask freeModeSelectables;
     const float maxRayCastDistance = 1000.0f;
     BuildingsManager.BuildingProposal buildingToPlace = null;
     Vector3 outOfViewPosition = new Vector3(0.0f, -10.0f, 0.0f); //Planned constructions will be moved to this location when the cursor is pointed outside allowable construction zone (outside grid boundaries)
 
     CameraControl cameraControl;
+    [SerializeField] float mousePanDeadZone = 0.01f; //dead zone distance (in percentage of screen pixels of each access) inside which no mouse movement is considered for panning camera
+    [SerializeField] float mousePanMaxRange = 0.1f; //in percentage of screen resolution, the distance from centre of right click where no more speed is gained for panning.
+
 
     void Awake()
     {
@@ -51,11 +54,11 @@ public class CursorHandler : MonoBehaviour
 
 
 //Controls
-    //float middleButtonHoldTime = 0.0f;
-    //[SerializeField] float middleMoustHoldToPanTime = 0.5f;
+    public float rightClickHoldTime = 0.0f;
+    [SerializeField] float rightClickHoldToPanTime = 0.5f;
     Vector3 lastMiddleClickLocation;
-
-
+    Vector3 lastRightClickLocation;
+    Vector3 rotationOrigin = new Vector3();
 
     void FreeModeControl()
     {
@@ -68,11 +71,41 @@ public class CursorHandler : MonoBehaviour
         }
         else if (Input.GetMouseButtonDown(2))
         {
+            //save the last mouse position (which will be used to calculate rotation angles)
             lastMiddleClickLocation = Input.mousePosition;
+            
+            //compute rotation origin
+            RaycastHit hit = cameraControl.CastRay(gridLayer | ground, maxRayCastDistance);
+            if (hit.collider != null)
+                rotationOrigin = hit.point;
+            else
+            {
+                print("Rotating about Grid centre"); //test
+                rotationOrigin = Grid.grid.gameObject.transform.position;
+            }
         }
         else if (Input.GetMouseButton(2))
         {
-            CameraRotate();
+            CameraRotate(rotationOrigin);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            rightClickHoldTime = 0.0f;
+            lastRightClickLocation = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            rightClickHoldTime += Time.deltaTime;
+            if (rightClickHoldTime >= rightClickHoldToPanTime)
+            {
+                CameraDrag();
+            }
+        }        
+        else
+        {
+            //here we can safely allow zooming or move camera by moving mouse to screen edge.
+            CameraScrollZoom();
+            CameraEdgeMove();
         }
     }
 
@@ -128,32 +161,37 @@ public class CursorHandler : MonoBehaviour
 
 
 //Camera controls
-
-    void zoom()
+    void CameraScrollZoom()
     {
+        float scrollDelta = Input.mouseScrollDelta.y;
+        if (scrollDelta > 0.1f || scrollDelta < -0.1f)
+        {
+            scrollDelta = scrollDelta / Mathf.Abs(scrollDelta); //normalize the value
+            cameraControl.Zoom(scrollDelta);
+        }
+    }
 
+    void CameraEdgeMove()
+    {
+        //TODO implement this
     }
 
     void CameraDrag()
     {
+        Vector3 difference = Input.mousePosition - lastRightClickLocation;
+        
+        if (Mathf.Abs(difference.x) < mousePanDeadZone * Screen.width && Mathf.Abs(difference.y) < mousePanDeadZone * Screen.height)
+            return;
 
+        Vector2 velocity = new Vector2();
+        velocity.x = Mathf.Sign(difference.x) * Mathf.Min(Mathf.Abs(difference.x), mousePanMaxRange * Screen.width) /  (mousePanMaxRange * Screen.width);
+        velocity.y = Mathf.Sign(difference.y) * Mathf.Min(Mathf.Abs(difference.y), mousePanMaxRange * Screen.height) / (mousePanMaxRange * Screen.height);
+
+        cameraControl.Pan(velocity);
     }
 
-    void CameraRotate()
+    void CameraRotate(Vector3 rotationOrigin)
     {
-
-        //first compute rotation origin
-        
-        Vector3 rotationOrigin;
-        RaycastHit hit = CastRay(gridLayer | ground);
-        //if (hit.collider != null)
-            //rotationOrigin = hit.point;
-        //else
-        //{
-            //print("Rotating about Grid centre"); //test
-            rotationOrigin = Grid.grid.gameObject.transform.position;
-        //}
-
         //then compute rotation angle
         Vector3 mouseDir = Input.mousePosition - lastMiddleClickLocation;
         mouseDir.Normalize();
@@ -178,8 +216,6 @@ public class CursorHandler : MonoBehaviour
         lastHitPosition = hit.point; //test. For vizualization in editor.
         return hit;
     }
-
-
 
 //Other Utilities
     public void SwitchToBuildingPlacement(BuildingsManager.BuildingProposal building)
