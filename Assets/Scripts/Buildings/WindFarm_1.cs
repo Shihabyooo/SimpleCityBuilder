d31b42a37888913b711626d903d000e00afdec79
Shihabyooo <shihabnoor@hotmail.com>
2020-06-15 16:23:45 +0200

    Orientation of wind farm relative to wind direction now affects efficiency.

--- Assets/Scripts/Buildings/WindFarm_1.cs
+++ Assets/Scripts/Buildings/WindFarm_1.cs
@@ -34,15 +34,46 @@ public class WindFarm_1 : InfrastructureBuilding
 
     protected override float ComputeEfficiency()
     {
-        //TODO adjust windspeed for direction here.
+        
         float cellWindSpeed = Grid.grid.windSpeedLayer.GetCellValue(occupiedCell[0], occupiedCell[1]);
-        uint cellWindDirection = Grid.grid.windDirectionLayer.GetCellValue(occupiedCell[0], occupiedCell[1]);
+        int cellWindDirection = (int)Grid.grid.windDirectionLayer.GetCellValue(occupiedCell[0], occupiedCell[1]); //Wind direction should be limited between 0 and 360, this casting should be safe.
+        //  Note that localRot returns value from 0 to 360 always.
+        //The code bellow assumes that localRot.y = 0 for a a wind farm model facing north (equivalent to windDir = 270)
+        //Note that "turbineFacing" bellow calculates the inverse facing, that is, the direction the end of the turbine is pointing at.
 
-        float efficiency = base.ComputeEfficiency() *  Mathf.Clamp(cellWindSpeed / farmStats.maxWindSpeed, 0.0f, 1.0f);
+        int turbineFacing = Mathf.RoundToInt( this.transform.eulerAngles.y);// - 270;  //TODO remember to adjust this 90 value if changed the wind farm's model 0 rotation facing.
+
+        // if (turbineFacing < 0)
+        // {
+        //     turbineFacing = (Mathf.FloorToInt(Mathf.Abs((float)turbineFacing / 360.0f)) + 1) * 360 + turbineFacing;
+        // }
+        // else if (turbineFacing > 360)
+        // {
+        //     turbineFacing = turbineFacing - (Mathf.FloorToInt((float)turbineFacing / 360.0f) ) * 360;
+        // }
+
+        print (this.gameObject.name + "  || facing: " + turbineFacing + ", localRot: " + this.transform.eulerAngles.y); //test
+        //int angleDifference = Mathf.Abs(cellWindDirection - turbineFacing) - 90;
+        int angleDifference = (Mathf.Max(cellWindDirection, Mathf.Abs(turbineFacing - 90)) - Mathf.Min(cellWindDirection, Mathf.Abs(turbineFacing - 90))) % 360;
+        angleDifference = angleDifference > 180? 360 - angleDifference : angleDifference;
+        
+        if (cellWindSpeed < farmStats.minWindSpeed || angleDifference > farmStats.maxWindSpeed)
+        {
+            print ("returning 0.0f for farm " + this.gameObject.name + ", difference: " + angleDifference); //test
+            return 0.0f;
+        }
+
+        float windDirectionEffect = (Mathf.Max(angleDifference, farmStats.maxPeakPerformanceAngleDifference) - (float)farmStats.maxOperationalAngleDifference) / ((float)farmStats.maxPeakPerformanceAngleDifference - (float)farmStats.maxOperationalAngleDifference) ;
+
+        float windSpeedEffect = Mathf.Min(cellWindSpeed / farmStats.maxWindSpeed, 1.0f);
+
+        float efficiency = windDirectionEffect * windSpeedEffect * base.ComputeEfficiency();
 
         if (efficiency > 0.001f)
             efficiency = Mathf.Max(efficiency, infraStats.minEfficiency);
 
+
+        print (this.gameObject.name + "  || windDirectionEffect: " + windDirectionEffect + ", difference: " + angleDifference); //test
         return efficiency;
     }
 
@@ -61,5 +92,6 @@ public class WindFarmStats
     public float maxPowerProduction = 0.0f; //unit power.
     public float minWindSpeed = 0.0f; //unit speed (adjusted with direction), minimum speed for the farm to operate.
     public float maxWindSpeed = 0.0f; //unit speed (adjusted with direciton), maximum operatational wind speed, any excess speed will not generate any more power.
-
+    public uint maxPeakPerformanceAngleDifference = 5; //in degrees, the angle difference between turbine facing and wind that would generate max performance, and greater difference reduces performance.
+    public uint maxOperationalAngleDifference = 55; //the maximum angle difference that the turbine can operate at. Any greater difference and the turbine won't operate.
 }
