dc6ab52a3ab09caaa0f1bc43360c40d627c6ee7a
Shihabyooo <shihabnoor@hotmail.com>
2020-06-27 18:47:18 +0200

    Added method and parameters for computing job quality based on buidlings budget

--- Assets/Scripts/WorkPlace.cs
+++ Assets/Scripts/WorkPlace.cs
@@ -9,7 +9,10 @@ public class WorkPlace : MonoBehaviour
     [SerializeField] uint maxManpower = 0; //maximum amount of workers this building can handle. Production is max when currentWorkersCount = maxWorker
     [SerializeField] EducationLevel minWorkerEducationLevel = EducationLevel.secondery;
     [SerializeField] uint wages = 0; //in units of money per day.
+    [SerializeField] [Range(20, 50)] uint minWorkplaceQuality = 35;
+    [SerializeField] [Range(70, 100)] uint maxWorkplaceQuality = 85;
     [SerializeField] protected List<Citizen> employees = new List<Citizen>(); //TODO remove serialization when testing is done.
+    uint workplaceQuality;
 
 //getters
     public uint AvailableWorkerSlots()
@@ -47,6 +50,17 @@ public class WorkPlace : MonoBehaviour
         return wages;
     }
 
+    public uint WorkplaceQuality()
+    {
+        //get budget from building, compute the percentage relative to min and max.
+        Building building = this.gameObject.GetComponent<Building>();
+        float budgetPercent = (building.Budget() - building.GetStats().minBudget) / (building.GetStats().maxBudget - building.GetStats().minBudget);
+
+        //compute quality based on budget
+        uint quality = (uint)Mathf.RoundToInt(budgetPercent * (maxWorkplaceQuality - minWorkplaceQuality) + minWorkplaceQuality);
+        
+        return quality;
+    }
 //setters
     public void AssignEmployee(Citizen citizen) //this method DOES NOT check that the assignment is valid (there are empty slots, citizen satisfies education requirement)
     {
