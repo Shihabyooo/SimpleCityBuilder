3c104712ed46501157830fc9b795a3829de102a6
Shihabyooo <shihabnoor@hotmail.com>
2020-07-01 16:13:25 +0200

    Changed Building buttons to be controlled through scripts that adds a method to the Unity's button's OnClick

---
+++ Assets/Scripts/UI/BuildingButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingButton : MonoBehaviour
+{
+    [SerializeField] int buildingID;
+
+    void Awake()
+    {
+        this.gameObject.GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        //print ("Clicked from " + this.gameObject.name);
+        GameManager.gameMan.SwitchToBuildingPlacement(buildingID);
+    }
+
+}
