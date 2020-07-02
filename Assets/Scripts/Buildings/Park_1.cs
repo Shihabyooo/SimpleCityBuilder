9ff003c154f8ac34edfa5a4907c99b10a745072d
Shihabyooo <shihabnoor@hotmail.com>
2020-07-02 13:03:31 +0200

    Implemented basic methods, added stats class.

--- Assets/Scripts/Buildings/Park_1.cs
+++ Assets/Scripts/Buildings/Park_1.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Parks reduce pollution to some extent, and grant a minimum boost to environment happiness.
+
 [RequireComponent(typeof(WorkPlace))]
 public class Park_1 : InfrastructureBuilding
 {
+    [SerializeField] ParkStats parkStats;
+    [SerializeField] ulong id; //TODO remove serialization after testing
+
     override protected void Awake()
     {
         base.Awake();
@@ -14,4 +19,51 @@ public class Park_1 : InfrastructureBuilding
     {
         BuildingDataViewer.viewerHandler.Show(this, BuildingViewerTemplate.park);
     }
+
+    protected override void OnConstructionComplete()
+    {
+        base.OnConstructionComplete();
+        Grid.grid.SetInfrastructureLayerCoverageByID(occupiedCell[0], occupiedCell[1], infraStats.radiusOfInfluence, InfrastructureService.parks, id);
+    }
+
+    //Because BuildingIDHandler in Buildings manager will also be used to track whether player has reached cap of builing of specific time, we need to reserve and get an ID
+    //as soon as player clicks to build a park, BeginConstruction() is called by BuildingPlan to mark this event, we override this method to add ID reserving functionality.
+    public override void BeginConstruction(Cell cell)
+    {
+        base.BeginConstruction(cell);
+        id = GameManager.buildingsMan.GetNewID(InfrastructureService.parks);
+    }
+
+    public override void UpdateEffectOnNature(int timeWindow)
+    {
+        base.UpdateEffectOnNature(timeWindow);
+        
+        //TODO Override UpdateEffectOnNature() to reduce pollution here.
+    }
+
+    //TODO efficiency calculated similarily to standard infrascture
+        //reduction of pollution (assuming minimum operation is satisfied) according to efficiency, interp between min and max.
+        //Ditto to happiness boost, which is stored as a fixed value (with a public getter), and polled by each citizen's happiness update loop (in population manager).
+
+    //TODO add a a limit to how many parks we can have (32 or 64), tracked in buildings manager and have button disabled when limit reached (or a message when attempting to
+    //construct a new park after limit).
+    //Then, have a GridLayer<uint> (or GridLayer<ulong> for 64 limit) each encoding which parks the said cell are serviced with, using bitwise ops.
+    //This, of course, means that each park should have an ID of 2^n, n from 0 to 32 (or 64). Buildings manager also needs to keep track of IDs used (could also use bitwise 
+    //ops and masks), OR'ing in an ID when a park constructed and assigned the said ID, and AND'ing it out when destroyed/removed.
+
+    //Try to make this system as modular and generic as possible, might convert other buildings/services to follow the same patterns.
+
+
+}
+
+[System.Serializable]
+public class ParkStats
+{
+    [Range(0, 4)] public uint minEnvironmentHappinessBoost;
+    [Range(6, 50)] public uint maxEnvironmentHappinessBoost;
+
+    //[Range(0, 4)]
+    public float minPollutionReduction;
+    //[Range(6, 50)]
+    public float maxPollutionReduction;
 }
