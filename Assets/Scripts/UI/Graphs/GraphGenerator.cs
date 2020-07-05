0f23e4c0dfed8ba023ddcfab31e36eda119c9410
Shihabyooo <shihabnoor@hotmail.com>
2020-07-05 14:25:47 +0200

    Modified Graph Generator to render to a texture, switched from immediate mode rendering to a on-demand rendering

--- Assets/Scripts/UI/Graphs/GraphGenerator.cs
+++ Assets/Scripts/UI/Graphs/GraphGenerator.cs
@@ -13,7 +13,7 @@ public class GraphGenerator : MonoBehaviour
     [SerializeField] Material bgMaterial;
     [SerializeField] Material decorationMaterial;
     [SerializeField] Vector2 graphSize;
-    [SerializeField] Vector2 referenceCanvasResolution;
+    //[SerializeField] Vector2 referenceCanvasResolution;
     [SerializeField] Vector2 padding;
 
     [SerializeField] float graphLineThickness = 2.0f; //in pixels.
@@ -25,10 +25,12 @@ public class GraphGenerator : MonoBehaviour
     Vector2 centre;
     GraphData activeGraphData = null;
     bool isShown = false;
+    RawImage viewport;
 
     void Awake()
     {
-        
+        viewport = this.gameObject.GetComponent<RawImage>();
+        //TODO set the rect trasnfrom width and height to the GraphSize here
     }
 
     public bool ShowGraph(TimeSeries<float> data)
@@ -36,7 +38,8 @@ public class GraphGenerator : MonoBehaviour
         if (!PrepareGraphData(data))
             return false;
 
-        CameraControl.postRender += DrawGraph;
+        //CameraControl.postRender += DrawGraphWindow;
+        DrawGraphWindow();
         isShown = true;
         return true;
     }
@@ -46,7 +49,7 @@ public class GraphGenerator : MonoBehaviour
         if (!isShown)
             return;
 
-        CameraControl.postRender -= DrawGraph;
+        //CameraControl.postRender -= DrawGraphWindow;
         isShown = false;
     }
 
@@ -64,13 +67,33 @@ public class GraphGenerator : MonoBehaviour
         return true;
     }
 
-    void DrawGraph()
+    [SerializeField] Color clearColour;
+    void DrawGraphWindow()
     {
         if(activeGraphData == null)
             return;
 
-        centre = this.gameObject.GetComponent<RectTransform>().anchoredPosition;
-        centre += referenceCanvasResolution / 2.0f;
+        viewport.texture = ReDrawGraph();
+
+    }
+
+    public Texture2D ReDrawGraph()
+    {
+        //Texture prep
+        //Source for method used to rendering to texture: https://forum.unity.com/threads/rendering-gl-to-a-texture2d-immediately-in-unity4.158918/
+        int texWidth = Mathf.CeilToInt(graphSize.x);
+        int texHeight = Mathf.CeilToInt(graphSize.y);
+        RenderTexture renderTexture = RenderTexture.GetTemporary(texWidth, texHeight);
+        RenderTexture.active = renderTexture;
+        GL.Clear(false, true, clearColour);
+
+
+        //Other prep
+        // centre = this.gameObject.GetComponent<RectTransform>().anchoredPosition;
+        // centre += referenceCanvasResolution / 2.0f;
+        centre = graphSize / 2.0f;
+
+        //GL prep and exec
         GL.PushMatrix();
         GL.LoadOrtho();
 
@@ -78,6 +101,15 @@ public class GraphGenerator : MonoBehaviour
         DrawContent(activeGraphData);
 
         GL.PopMatrix();
+
+        //Finish up the render-to-texture stuff
+        Texture2D outputTexture = new Texture2D(texWidth, texHeight);
+        outputTexture.ReadPixels(new Rect(0, 0, texWidth, texHeight), 0, 0);
+        outputTexture.Apply(false);
+        outputTexture.Compress(true);
+        RenderTexture.active = null;
+        RenderTexture.ReleaseTemporary(renderTexture);
+        return outputTexture;
     }
 
     void DrawBackground()
@@ -86,13 +118,17 @@ public class GraphGenerator : MonoBehaviour
         GL.Begin(GL.QUADS);
         
         GL.TexCoord2(0.0f, 0.0f);
-        GL.Vertex3((centre.x - graphSize.x / 2.0f) / referenceCanvasResolution.x, (centre.y - graphSize.y / 2.0f) / referenceCanvasResolution.y, 0.0f);
+        //GL.Vertex3((centre.x - graphSize.x / 2.0f) / referenceCanvasResolution.x, (centre.y - graphSize.y / 2.0f) / referenceCanvasResolution.y, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 0.0f);
         GL.TexCoord2(0.0f, 1.0f);
-        GL.Vertex3((centre.x - graphSize.x / 2.0f) / referenceCanvasResolution.x, (centre.y + graphSize.y / 2.0f) / referenceCanvasResolution.y, 0.0f);
+        //GL.Vertex3((centre.x - graphSize.x / 2.0f) / referenceCanvasResolution.x, (centre.y + graphSize.y / 2.0f) / referenceCanvasResolution.y, 0.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
         GL.TexCoord2(1.0f, 1.0f);
-        GL.Vertex3((centre.x + graphSize.x / 2.0f) / referenceCanvasResolution.x, (centre.y + graphSize.y / 2.0f) / referenceCanvasResolution.y, 0.0f);
+        //GL.Vertex3((centre.x + graphSize.x / 2.0f) / referenceCanvasResolution.x, (centre.y + graphSize.y / 2.0f) / referenceCanvasResolution.y, 0.0f);
+        GL.Vertex3(1.0f, 1.0f, 0.0f);
         GL.TexCoord2(1.0f, 0.0f);
-        GL.Vertex3((centre.x + graphSize.x / 2.0f) / referenceCanvasResolution.x, (centre.y - graphSize.y / 2.0f) / referenceCanvasResolution.y, 0.0f);
+        //GL.Vertex3((centre.x + graphSize.x / 2.0f) / referenceCanvasResolution.x, (centre.y - graphSize.y / 2.0f) / referenceCanvasResolution.y, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 0.0f);
 
         GL.End();
     }
@@ -111,10 +147,14 @@ public class GraphGenerator : MonoBehaviour
         decorationMaterial.SetPass(0);
         
         //Cache some repeating calculations
-        float relOriginX = origin.x/ referenceCanvasResolution.x;
-        float relOriginY = origin.y / referenceCanvasResolution.y;
-        float relThicknessHalfX = axesLineThickness / (referenceCanvasResolution.x * 2.0f);
-        float relThicknessHalfY = axesLineThickness / (referenceCanvasResolution.y * 2.0f);
+        //float relOriginX = origin.x / referenceCanvasResolution.x;
+        float relOriginX = origin.x / graphSize.x;
+        //float relOriginY = origin.y / referenceCanvasResolution.y;
+        float relOriginY = origin.y / graphSize.y;
+        //float relThicknessHalfX = axesLineThickness / (referenceCanvasResolution.x * 2.0f);
+        float relThicknessHalfX = axesLineThickness / (graphSize.x * 2.0f);
+        //float relThicknessHalfY = axesLineThickness / (referenceCanvasResolution.y * 2.0f);
+        float relThicknessHalfY = axesLineThickness / (graphSize.y * 2.0f);
 
         //Draw Axes
         GL.Begin(GL.QUADS);
@@ -122,40 +162,51 @@ public class GraphGenerator : MonoBehaviour
         //Y axis always begins at left edge        
         GL.Vertex3(relOriginX + relThicknessHalfX,  relOriginY, 0.0f );
         GL.Vertex3(relOriginX - relThicknessHalfX,  relOriginY, 0.0f );
-        GL.Vertex3(relOriginX - relThicknessHalfX,  (origin.y + workingDimensions.y) / referenceCanvasResolution.y, 0.0f );
-        GL.Vertex3(relOriginX + relThicknessHalfX,  (origin.y + workingDimensions.y) / referenceCanvasResolution.y, 0.0f );
+        //GL.Vertex3(relOriginX - relThicknessHalfX,  (origin.y + workingDimensions.y) / referenceCanvasResolution.y, 0.0f );
+        GL.Vertex3(relOriginX - relThicknessHalfX,  (origin.y + workingDimensions.y) / graphSize.y, 0.0f );
+        //GL.Vertex3(relOriginX + relThicknessHalfX,  (origin.y + workingDimensions.y) / referenceCanvasResolution.y, 0.0f );
+        GL.Vertex3(relOriginX + relThicknessHalfX,  (origin.y + workingDimensions.y) / graphSize.y, 0.0f );
 
         //The X axis is clamped to the zero value (if the values oscilate between positive and negative), or is the min (for all positive) or max (for all negative) value.
         float yCoord;
         if (data.isOscillatingAroundYAxis) //compute the position of y=zero.
-            yCoord = (origin.y + workingDimensions.y * (0.0f - data.minY) / data.rangeY) / referenceCanvasResolution.y;
+            //yCoord = (origin.y + workingDimensions.y * (0.0f - data.minY) / data.rangeY) / referenceCanvasResolution.y;
+            yCoord = (origin.y + workingDimensions.y * (0.0f - data.minY) / data.rangeY) / graphSize.y;
         else //compute position depending on whether all positive or negative
-            yCoord = (origin.y + Mathf.Min((Mathf.Sign(data.Value(0))) * workingDimensions.y , 0.0f)) / referenceCanvasResolution.y;
+            //yCoord = (origin.y + Mathf.Min((Mathf.Sign(data.Value(0))) * workingDimensions.y , 0.0f)) / referenceCanvasResolution.y;
+            yCoord = (origin.y + Mathf.Min((Mathf.Sign(data.Value(0))) * workingDimensions.y , 0.0f)) / graphSize.y;
         
         GL.Vertex3(relOriginX, yCoord - relThicknessHalfY, 0.0f);
         GL.Vertex3(relOriginX, yCoord + relThicknessHalfY, 0.0f);
-        GL.Vertex3(relOriginX + workingDimensions.x / referenceCanvasResolution.x, yCoord + relThicknessHalfY, 0.0f);
-        GL.Vertex3(relOriginX + workingDimensions.x / referenceCanvasResolution.x, yCoord - relThicknessHalfY, 0.0f);
+        //GL.Vertex3(relOriginX + workingDimensions.x / referenceCanvasResolution.x, yCoord + relThicknessHalfY, 0.0f);
+        GL.Vertex3(relOriginX + workingDimensions.x / graphSize.x, yCoord + relThicknessHalfY, 0.0f);
+        //GL.Vertex3(relOriginX + workingDimensions.x / referenceCanvasResolution.x, yCoord - relThicknessHalfY, 0.0f);
+        GL.Vertex3(relOriginX + workingDimensions.x / graphSize.x, yCoord - relThicknessHalfY, 0.0f);
 
 
         //Draw Grid
         //modify cached reThicknessHalfX and Y to use
-        relThicknessHalfX = gridLineThickness / (referenceCanvasResolution.x * 2.0f);
-        relThicknessHalfY = gridLineThickness / (referenceCanvasResolution.y * 2.0f);
+        //relThicknessHalfX = gridLineThickness / (referenceCanvasResolution.x * 2.0f);
+        relThicknessHalfX = gridLineThickness / (graphSize.x * 2.0f);
+        //relThicknessHalfY = gridLineThickness / (referenceCanvasResolution.y * 2.0f);
+        relThicknessHalfY = gridLineThickness / (graphSize.y * 2.0f);
 
         int xGridCount = Mathf.Min(data.Length(), maxGridCountX);
         int yGridCount = Mathf.Min(data.Length(), maxGridCountY);
 
         float daysPerGridSpacing = Mathf.Floor(data.Length() / xGridCount);
-        float xDistPerDay = (workingDimensions.x / referenceCanvasResolution.x) / (float)data.Length();
+        //float xDistPerDay = (workingDimensions.x / referenceCanvasResolution.x) / (float)data.Length();
+        float xDistPerDay = (workingDimensions.x / graphSize.x) / (float)data.Length();
         float distPerGridSpacingX = daysPerGridSpacing * xDistPerDay;
 
         for (int i = 1; i < xGridCount; i++)
         {
             GL.Vertex3(relOriginX + (i * distPerGridSpacingX) + relThicknessHalfX, relOriginY, 0.0f);
             GL.Vertex3(relOriginX + (i * distPerGridSpacingX) - relThicknessHalfX, relOriginY, 0.0f);
-            GL.Vertex3(relOriginX + (i * distPerGridSpacingX) - relThicknessHalfX, (origin.y + workingDimensions.y) / referenceCanvasResolution.y, 0.0f);
-            GL.Vertex3(relOriginX + (i * distPerGridSpacingX) + relThicknessHalfX, (origin.y + workingDimensions.y) / referenceCanvasResolution.y, 0.0f);
+            //GL.Vertex3(relOriginX + (i * distPerGridSpacingX) - relThicknessHalfX, (origin.y + workingDimensions.y) / referenceCanvasResolution.y, 0.0f);
+            GL.Vertex3(relOriginX + (i * distPerGridSpacingX) - relThicknessHalfX, (origin.y + workingDimensions.y) / graphSize.y, 0.0f);
+            //GL.Vertex3(relOriginX + (i * distPerGridSpacingX) + relThicknessHalfX, (origin.y + workingDimensions.y) / referenceCanvasResolution.y, 0.0f);
+            GL.Vertex3(relOriginX + (i * distPerGridSpacingX) + relThicknessHalfX, (origin.y + workingDimensions.y) / graphSize.y, 0.0f);
         }
 
 
@@ -178,8 +229,10 @@ public class GraphGenerator : MonoBehaviour
             for (int j = i - 1; j <= i ; j++)
             {
                 //Compute X relative position
-                float xCoord =  (origin.x + workingDimensions.x * ((float)data.DaysSinceStart(j)) / (float)data.rangeX) / referenceCanvasResolution.x;
-                float yCoord = (origin.y + workingDimensions.y * (data.Value(j) - data.minY) / data.rangeY) / referenceCanvasResolution.y;
+                //float xCoord =  (origin.x + workingDimensions.x * ((float)data.DaysSinceStart(j)) / (float)data.rangeX) / referenceCanvasResolution.x;
+                float xCoord =  (origin.x + workingDimensions.x * ((float)data.DaysSinceStart(j)) / (float)data.rangeX) / graphSize.x;
+                //float yCoord = (origin.y + workingDimensions.y * (data.Value(j) - data.minY) / data.rangeY) / referenceCanvasResolution.y;
+                float yCoord = (origin.y + workingDimensions.y * (data.Value(j) - data.minY) / data.rangeY) / graphSize.y;
                 
                 //Set the vertex
                 GL.Vertex3(xCoord, yCoord, 0.0f);
@@ -190,7 +243,8 @@ public class GraphGenerator : MonoBehaviour
 
     //test
     public Vector2[] testData;
-    void OnValidate()
+    //void OnValidate()
+    public void TestShow()
     {
         if (UnityEditor.EditorApplication.isPlaying && testData != null)
         {
