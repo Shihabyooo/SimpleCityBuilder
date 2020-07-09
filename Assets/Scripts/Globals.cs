6652f6d101c0d0fc27cb4aa37abc2978ac528640
Shihabyooo <shihabnoor@hotmail.com>
2020-07-09 14:35:32 +0200

    Added a script to contain all global consts and parameters

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script is mostly dedicated to consts used throughout other scipts.
+
+public class Globals
+{
+    public const float minPollution = 0.0f;
+    public const float maxPollution = 1000000.0f;
+}
