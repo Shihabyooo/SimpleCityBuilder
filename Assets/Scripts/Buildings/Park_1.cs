ec95594f89c8f76405dd42d07a736bb805e8b265
Shihabyooo <shihabnoor@hotmail.com>
2020-07-10 14:19:09 +0200

    Added environmentHappinessBoost calculation, removed local ID retention and moved to base InfrastructureBuilding class, some cleanup and minor modifications.

--- Assets/Scripts/Buildings/Park_1.cs
+++ Assets/Scripts/Buildings/Park_1.cs
@@ -8,7 +8,6 @@ using UnityEngine;
 public class Park_1 : InfrastructureBuilding
 {
     [SerializeField] ParkStats parkStats;
-    [SerializeField] ulong id; //TODO remove serialization after testing
 
     public uint environmentHappinessBoost {get; private set;}
 
@@ -30,15 +29,7 @@ public class Park_1 : InfrastructureBuilding
         Grid.grid.SetInfrastructureLayerCoverageByID(occupiedCell[0], occupiedCell[1], infraStats.radiusOfInfluence, InfrastructureService.parks, id);
     }
 
-    //Because BuildingIDHandler in Buildings manager will also be used to track whether player has reached cap of builing of specific time, we need to reserve and get an ID
-    //as soon as player clicks to build a park, BeginConstruction() is called by BuildingPlan to mark this event, we override this method to add ID reserving functionality.
-    public override void BeginConstruction(Cell cell)
-    {
-        base.BeginConstruction(cell);
-        id = GameManager.buildingsMan.GetNewID(InfrastructureService.parks);
-    }
-
-    public override void UpdateEffectOnNature(int timeWindow)
+    public override void UpdateEffectOnNature(int timeWindow) //Update effect on nature is called every sim update. So we can use it to calculate environment boost as well (less code)
     {
         base.UpdateEffectOnNature(timeWindow);
         
@@ -47,21 +38,32 @@ public class Park_1 : InfrastructureBuilding
         pollutionReduction = -1.0f * pollutionReduction; //Because we will subtract it from the current pollution
 
         Grid.grid.SetPollutionCummilative(occupiedCell[0], occupiedCell[1], infraStats.radiusOfInfluence, pollutionReduction);
+
+        CalculateEnvironmentHappinessBoost();
     }
 
-    //TODO efficiency calculated similarily to standard infrascture
-        //reduction of pollution (assuming minimum operation is satisfied) according to efficiency, interp between min and max.
-        //Ditto to happiness boost, which is stored as a fixed value (with a public getter), and polled by each citizen's happiness update loop (in population manager).
+    void CalculateEnvironmentHappinessBoost()
+    {
+        float efficiency = ComputeEfficiency();
+        environmentHappinessBoost = (uint)Mathf.RoundToInt(efficiency * (float)(parkStats.maxEnvironmentHappinessBoost - parkStats.minEnvironmentHappinessBoost) + parkStats.minEnvironmentHappinessBoost);
+    }
 
-    //TODO add a a limit to how many parks we can have (32 or 64), tracked in buildings manager and have button disabled when limit reached (or a message when attempting to
-    //construct a new park after limit).
-    //Then, have a GridLayer<uint> (or GridLayer<ulong> for 64 limit) each encoding which parks the said cell are serviced with, using bitwise ops.
-    //This, of course, means that each park should have an ID of 2^n, n from 0 to 32 (or 64). Buildings manager also needs to keep track of IDs used (could also use bitwise 
-    //ops and masks), OR'ing in an ID when a park constructed and assigned the said ID, and AND'ing it out when destroyed/removed.
+    static public uint EnvironmentBoostMax(uint cellID_x, uint cellID_y) //Requires an active Grid and BuildingsManager (which should always exist in this game, anyway)
+    {   
+        int maxBoost = 0;
 
-    //Try to make this system as modular and generic as possible, might convert other buildings/services to follow the same patterns.
+        List<ulong> coveringParks = Grid.grid.GetInfrastructureIDsCoveringCell(cellID_x, cellID_y, InfrastructureService.parks);
 
+        foreach (ulong id in coveringParks)
+        {
+            Park_1 park = (Park_1)GameManager.buildingsMan.GetInfrastructureBuilding(InfrastructureService.parks, id);
+            
+            if (park != null)
+                maxBoost = Mathf.Max((int)park.environmentHappinessBoost, maxBoost);
+        }
 
+        return (uint)maxBoost;
+    }
 }
 
 [System.Serializable]
