d3676329fcf596659baf885b023d1f677b0726d4
Shihabyooo <shihabnoor@hotmail.com>
2020-07-11 16:05:31 +0200

    Added some basic methods and variables.

--- Assets/Scripts/Buildings/PoliceStation_1.cs
+++ Assets/Scripts/Buildings/PoliceStation_1.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(WorkPlace))]
 public class PoliceStation_1 : InfrastructureBuilding
 {
+    [SerializeField] PoliceStationStats stationStats;
+    public uint crimeFightingCapacity {get; private set;}
+
    override protected void Awake()
     {
         base.Awake();
@@ -15,4 +18,22 @@ public class PoliceStation_1 : InfrastructureBuilding
     {
         BuildingDataViewer.viewerHandler.Show(this, BuildingViewerTemplate.police);
     }
+
+    protected override void OnConstructionComplete()
+    {
+        base.OnConstructionComplete();
+        Grid.grid.SetInfrastructureLayerCoverageByID(occupiedCell[0], occupiedCell[1], infraStats.radiusOfInfluence, InfrastructureService.safety, id);
+    }
+
+    public override void UpdateEffectOnNature(int timeWindow) //Update effect on nature is called every sim update, using it here to recalculate crimeFightingCapacity without
+    {                                                         //adding more calls to sim manager.
+        crimeFightingCapacity = workPlace.CurrentManpower() * crimeFightingCapacity;
+    }
+}
+
+
+[System.Serializable]
+public class PoliceStationStats
+{
+    public uint crimeFightingCapacityPerOfficer = 10;
 }
